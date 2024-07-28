09d7191f8e31cb39f4a3d790a056a0b555edbe1c
Deepak Chaurasiya <deepak.chaurasiya@indusnet.co.in>
2024-07-29 01:18:14 +0530

    Solved Leetcode Problem - 1. https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150

---
+++ array/majority-element.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 169. Majority Element
+
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+	let element = nums[0];
+	let count = 1;
+	for (let i = 1; i < nums.length; i++) {
+		if (count === 0) {
+			element = nums[i];
+			count = 1;
+		}
+		else if (nums[i] === element) {
+			count++;
+		} else {
+			count--;
+		}
+	}
+    return element
+};
+
+let nums = [2,2,1,1,1,2,2]
+console.log(majorityElement(nums));

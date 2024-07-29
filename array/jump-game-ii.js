8b2464d00e8faf4c76b47e74393094c930754442
Deepak Chaurasiya <86410087+deepak-chaurasiyaa@users.noreply.github.com>
2024-07-30 01:19:58 +0530

    Update majority-element.js
    additional input change

---
+++ array/jump-game-ii.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/jump-game-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 45. Jump Game II
+
+// Example 1:
+
+// Input: nums = [2,3,1,1,4]
+// Output: 2
+// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+
+// Input: nums = [2,3,0,1,4]
+// Output: 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {};

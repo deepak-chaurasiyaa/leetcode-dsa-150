4cd3af9a32d4ff7964459f422dd442ec6832dbf0
Deepak Chaurasiya <deepak.chaurasiya@indusnet.co.in>
2024-07-30 23:26:46 +0530

    Solved Leetcode Problem 45. Jump Game II

--- array/jump-game-ii.js
+++ array/jump-game-ii.js
@@ -16,4 +16,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var jump = function (nums) {};
+var jump = function (nums) {
+	let [res, left, right] = [0, 0, 0];
+	while (right < nums.length - 1) {
+		let farthest = 0;
+		for (let i = left; i <= right; i++) {
+			farthest = Math.max(farthest, i + nums[i]);
+		}
+		left = right + 1;
+		right = farthest;
+		res += 1;
+	}
+	return res;
+};
+
+const nums =  [2,3,1,1,4]
+
+console.log(jump(nums));
